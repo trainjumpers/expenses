965fbee657880f94a89a7a60f7f204e9f2b7c25f
Pranav <npv12@iitbbs.ac.in>
2024-04-15 11:37:34 +0530

    chore: add a 404 page

---
+++ frontend/src/views/NotFoundView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="notFound">
+    <h1>404 Not Found</h1>
+    <p>The page you are looking for does not exist.</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .notFound {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+  }
+}
+</style>
