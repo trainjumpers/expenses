d07fbb37c3e5e81506e1f21b92759eba24d13cbc
Pranav <pranav10121@gmail.com>
2025-01-08 08:59:42 +0530

    chore: supply port
    Signed-off-by: Pranav <pranav10121@gmail.com>

--- server/go.mod
+++ server/go.mod
@@ -5,7 +5,6 @@ go 1.23.4
 require (
 	github.com/gin-gonic/gin v1.10.0
 	github.com/golang-jwt/jwt/v5 v5.2.1
-	github.com/joho/godotenv v1.5.1
 	go.uber.org/zap v1.27.0
 )
 
--- server/main.go
+++ server/main.go
@@ -3,19 +3,13 @@ package main
 import (
 	database "expenses/db"
 	"expenses/routes"
-	"fmt"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		fmt.Println(err)
-	}
-
 	database.ConnectDatabase()
 	r := routes.Init()
 
@@ -24,5 +18,6 @@ func main() {
 		c.String(200, "Hello, you've requested: %s at %s\n", c.Request.URL.Path, t.UTC().Format("2006-01-02 15:04:05.00 -0700 MST"))
 	})
 
-	r.Run("localhost:8080")
+	port := os.Getenv("PORT")
+	r.Run("localhost:" + port)
 }
