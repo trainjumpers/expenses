1c999b1286b1c3968eebe80c36f90d5971513d6b
Pranav <pranav10121@gmail.com>
2025-05-18 10:34:58 +0530

    chore: improve logger to use prod configs
    Signed-off-by: Pranav <pranav10121@gmail.com>

--- server/pkg/logger/logger.go
+++ server/pkg/logger/logger.go
@@ -1,6 +1,8 @@
 package logger
 
 import (
+	"os"
+
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
@@ -9,15 +11,45 @@ var Info, Error, Debug, Warn, Fatal func(args ...interface{})
 var Infof, Errorf, Debugf, Warnf, Fatalf func(template string, args ...interface{})
 
 func init() {
-	config := zap.NewDevelopmentConfig()
-	config.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
-	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
-	config.EncoderConfig.TimeKey = "timestamp"
-	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-	config.EncoderConfig.CallerKey = "caller"
-	config.EncoderConfig.EncodeCaller = zapcore.ShortCallerEncoder
+	env := os.Getenv("ENV")
+	var level zap.AtomicLevel
+	if env == "" {
+		env = "development"
+	}
+	if env == "development" {
+		level = zap.NewAtomicLevelAt(zap.DebugLevel)
+	} else {
+		level = zap.NewAtomicLevelAt(zap.InfoLevel)
+	}
 
-	logger, _ := config.Build()
+	logger, err := zap.Config{
+		Level:       level,
+		Development: false,
+		Sampling: &zap.SamplingConfig{
+			Initial:    100,
+			Thereafter: 100,
+		},
+		Encoding: "console",
+		EncoderConfig: zapcore.EncoderConfig{
+			TimeKey:        "timestamp",
+			LevelKey:       "level",
+			NameKey:        "logger",
+			CallerKey:      "caller",
+			FunctionKey:    zapcore.OmitKey,
+			MessageKey:     "msg",
+			StacktraceKey:  "stacktrace",
+			LineEnding:     zapcore.DefaultLineEnding,
+			EncodeLevel:    zapcore.CapitalColorLevelEncoder,
+			EncodeTime:     zapcore.ISO8601TimeEncoder,
+			EncodeDuration: zapcore.SecondsDurationEncoder,
+			EncodeCaller:   zapcore.ShortCallerEncoder,
+		},
+		OutputPaths:      []string{"stderr"},
+		ErrorOutputPaths: []string{"stderr"},
+	}.Build()
+	if err != nil {
+		panic(err)
+	}
 	sugar := logger.Sugar()
 
 	Info = sugar.Info
