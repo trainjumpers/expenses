85138ac54daf56c4ab80ee8a02b21d515e7167ac
Pranav <pranav10121@gmail.com>
2025-05-28 17:46:13 +0530

    fix: improve cookies security
    Signed-off-by: Pranav <pranav10121@gmail.com>

--- frontend/lib/utils/cookies.ts
+++ frontend/lib/utils/cookies.ts
@@ -9,16 +9,25 @@ export function getCookie(name: string): string | undefined {
 export function setCookie(
   name: string,
   value: string,
-  options: { maxAge?: number } = {}
+  options: {
+    maxAge?: number;
+    secure?: boolean;
+    sameSite?: "Strict" | "Lax" | "None";
+  } = {}
 ) {
   let cookie = `${name}=${value}`;
   if (options.maxAge) {
     cookie += `; max-age=${options.maxAge}`;
   }
   cookie += "; path=/";
+  const sameSite = options.sameSite || "Strict";
+  cookie += `; SameSite=${sameSite}`;
+  if (options.secure !== false) {
+    cookie += "; Secure";
+  }
   document.cookie = cookie;
 }
 
 export function deleteCookie(name: string) {
-  document.cookie = `${name}=; max-age=0; path=/`;
+  document.cookie = `${name}=; max-age=0; path=/; SameSite=Strict; Secure`;
 }
