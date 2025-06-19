0a3b981291942ddfde3c4e5539bb716c1ebb0873
Pranav <pranav10121@gmail.com>
2025-06-19 13:51:07 +0530

    fix: add a ;
    Signed-off-by: Pranav <pranav10121@gmail.com>

--- server/internal/database/seed/test/00003_categories.sql
+++ server/internal/database/seed/test/00003_categories.sql
@@ -6,7 +6,7 @@ INSERT INTO test.categories (name, icon, created_by, created_at, updated_at) VAL
 INSERT INTO test.categories (name, icon, created_by, created_at, updated_at) VALUES ('Shopping', 'shopping-icon', 1, NOW(), NOW());
 INSERT INTO test.categories (name, icon, created_by, created_at, updated_at) VALUES ('Health', 'health-icon', 1, NOW(), NOW());
 INSERT INTO test.categories (name, icon, created_by, created_at, updated_at) VALUES ('Other', 'other-icon', 2, NOW(), NOW());
-INSERT INTO test.categories (name, icon, created_by, created_at, updated_at) VALUES ('Salary', 'salary-icon', 2, NOW(), NOW())
+INSERT INTO test.categories (name, icon, created_by, created_at, updated_at) VALUES ('Salary', 'salary-icon', 2, NOW(), NOW());
 -- +goose StatementEnd
 
 -- +goose Down
