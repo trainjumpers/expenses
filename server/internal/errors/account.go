852926aba2cd4c39d0db3a3db2551e28a06d342d
Pranav <pranav10121@gmail.com>
2025-08-02 19:17:21 +0530

    fix: correct toast when deleting account with a txn
    Signed-off-by: Pranav <pranav10121@gmail.com>

--- server/internal/errors/account.go
+++ server/internal/errors/account.go
@@ -5,3 +5,7 @@ import "net/http"
 func NewAccountNotFoundError(err error) *AuthError {
 	return formatError(http.StatusNotFound, "account not found", err, "AccountNotFound")
 }
+
+func NewAccountHasTransactionsError(err error) *AuthError {
+	return formatError(http.StatusConflict, "cannot delete account with existing transactions", err, "AccountHasTransactions")
+}
