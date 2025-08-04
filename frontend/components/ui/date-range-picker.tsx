74be597a18c684a10656f042bb91a73388e63d1b
Pranav Nedungadi <59979143+npv12@users.noreply.github.com>
2025-08-04 19:37:53 +0530

    feat: datepicker: add all time (#91)

--- frontend/components/ui/date-range-picker.tsx
+++ frontend/components/ui/date-range-picker.tsx
@@ -80,6 +80,7 @@ const PRESETS: Preset[] = [
   { name: "lastWeek", label: "Last Week" },
   { name: "thisMonth", label: "This Month" },
   { name: "lastMonth", label: "Last Month" },
+  { name: "allTime", label: "All Time" },
 ];
 
 /** The DateRangePicker component allows a user to select a range of dates */
@@ -194,6 +195,12 @@ export const DateRangePicker: FC<DateRangePickerProps> & {
         from.setHours(0, 0, 0, 0);
         to.setDate(0);
         to.setHours(23, 59, 59, 999);
+        break;
+      case "allTime":
+        from.setFullYear(1970, 0, 1);
+        from.setHours(0, 0, 0, 0);
+        to.setHours(23, 59, 59, 999);
+        to.setDate(0);
         break;
     }
 
