aad736a87bf97a1017be93b1c17dcf666bad8431
Pranav <pranav10121@gmail.com>
2026-01-03 14:22:54 +0530

    fix: ui: theme provider not loading
    Signed-off-by: Pranav <pranav10121@gmail.com>

--- frontend/components/providers/ThemeProvider.tsx
+++ frontend/components/providers/ThemeProvider.tsx
@@ -1,21 +1,18 @@
 "use client";
 
-import { Skeleton } from "@/components/ui/skeleton";
-import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes";
+import dynamic from "next/dynamic";
 import * as React from "react";
+
+const NextThemesProvider = dynamic(
+  () => import("next-themes").then((e) => e.ThemeProvider),
+  {
+    ssr: false,
+  }
+);
 
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  const { resolvedTheme } = useTheme();
-
-  if (!resolvedTheme) {
-    return (
-      <div className="min-h-screen w-screen flex items-center justify-center bg-zinc-900">
-        <Skeleton className="h-20 w-20 rounded-2xl" />
-      </div>
-    );
-  }
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
